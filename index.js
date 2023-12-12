b7973b7fe2981c01ff53684d332c80132d3c7d0c
chrisfran03 <270155795@yoobeestudent.ac.nz>
2023-12-12 14:47:35 +1300

    add API call to get christmas joke

--- index.js
+++ index.js
@@ -8,9 +8,23 @@ app.use(express.static("public"));
 
 
 app.get("/", (req, res) => {
-
     res.render("index.ejs");
-})
+});
+
+app.get("/joke", async (req, res) => {
+    try {
+        const response = await axios.get("https://christmascountdown.live/api/joke");
+        const result = response.data;
+        res.render("joke.ejs", {
+            joke: result
+        });
+    } catch (error) {
+        console.error("Failed to make request:", error.message);
+        res.render("index.ejs", {
+            error: "Sorry the elves could not fetch you a joke.",
+        });
+    }
+});
 
 app.listen(port, () => {
     console.log(`Server running at http://127.0.0.1:${port}`);
